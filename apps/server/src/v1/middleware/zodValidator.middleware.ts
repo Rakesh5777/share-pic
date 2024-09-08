8b60c1d5147f4f1f97d6aa38d321a408851f2031
rakesh reddy <rakeshreddy.k5777@gmail.com>
2024-09-09 00:56:12 +0530

    feat: removed duplicate asyncWrapper

--- apps/server/src/v1/middleware/zodValidator.middleware.ts
+++ apps/server/src/v1/middleware/zodValidator.middleware.ts
@@ -1,19 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 
-type AsyncWrapperType = (
-  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
-) => (req: Request, res: Response, next: NextFunction) => Promise<any>;
-
-export const asyncWrapper = (schema: any): AsyncWrapperType => {
-  return function (
-    fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
-  ) {
-    return function (req: Request, res: Response, next: NextFunction) {
-      return fn(req, res, next).catch(next);
-    };
-  };
-};
-
 export const zodValidatorMiddleware = (schema: any) => {
   return (req: Request, _res: Response, next: NextFunction) => {
     schema.parse(req.body);
